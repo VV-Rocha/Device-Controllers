0af4a3607771ef647f86206c12c1fdaa162fcb0d
Vicente <vicentevrocha.vvr@gmail.com>
2023-09-21 17:20:51 +0100

    In parallel with the previous push we submitted the exposure of the circular mask function.

--- DMD/__init__.py
+++ DMD/__init__.py
@@ -1,5 +1,8 @@
-## Export function initiating master mode of the DMD
+## Export function initiating master mode of the DMD.
 from .DMD_initializer import InitiateMasterMode
 
-## Export the function used to allocate the masks in the DMD memory
+## Export the function used to allocate the masks in the DMD memory.
 from .DMD_initializer import AllocateMasks
+
+## Export repository of commonly used masks.
+from .Masks.masks import *
